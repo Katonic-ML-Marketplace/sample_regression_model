fe52c70d969809c190d8365cc78f36c64d43939a
AnuF0731 <anuja.fole@katonic.ai>
2023-04-18 06:24:47 +0000

    added regression model

---
+++ launch.py
@@ -0,0 +1,28 @@
+
+import pickle
+import numpy as np 
+import os
+from typing import Any, Union,Dict
+import os
+
+def loadmodel(logger):
+    """Get model from cloud object storage."""
+    logger.info("loading model")
+    TRAINED_MODEL_FILEPATH = f"regression.pickle"
+    with open(TRAINED_MODEL_FILEPATH , 'rb') as f:
+        clfdt = pickle.load(f)
+    return clfdt  
+
+def preprocessing(df:np.ndarray,logger):
+    """ Applies preprocessing techniques to the raw data"""
+    logger.info("no preprocessing")
+    return False
+    
+def predict(features: np.ndarray,model:Any,logger) -> Dict[str, str]:
+    """Predicts the results for the given inputs"""
+    logger.info("model prediction")
+    prediction = model.predict(features)
+    return prediction
+
+          
+   
